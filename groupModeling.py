80328b3b34453646779783d87092656701c7a894
jouuat <38770926+jouuat@users.noreply.github.com>
2020-11-24 11:20:38 +0100

    fai, xpo and spgreedy

--- groupModeling.py
+++ groupModeling.py
@@ -5,6 +5,7 @@ from metrics import metrics
 from recommender import recommendationModel
 from reputation import reputation
 import math
+import progressbar
 
 
 class groupModeling:
@@ -14,10 +15,15 @@ class groupModeling:
         self.metric = config.metric
         self.groups = groups
         self.numOfIterations = config.numOfIterations
+        self.usersPerGroup = config.usersPerGroup
         # for reputation
         self.ratings_pd = ratings_pd
         self.greedylmlambda = config.greedylmlambda
         self.p = config.p
+        self.mproportionality = config.mproportionality
+        self.numRelevantItems = config.numRelevantItems
+        if str(self.numRelevantItems).lower() == "auto":
+            self.numRelevantItems = self.numOfRecommendations
 
     def model(self, tfRecommender):
         # AVERAGE
@@ -27,17 +33,29 @@ class groupModeling:
         totalScores = list()
         recommendations = list()
 
+        bar = progressbar.ProgressBar(maxval=len(self.groups), widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+        bar.start()
+
         for group in range(len(self.groups)):
+            bar.update(group + 1)
             ids = self.groups[group]
             relMatrix = recommendationModel.predict(ids, tfRecommender)
+            # Penalize those films already seen by a user ( TODO MASSA LENT)
+            '''for id in relMatrix.columns:
+                minPuntuation = min(relMatrix[id])
+                for item in relMatrix.index:
+                    if ((train['user_id'] == id) & (train['movie_title'] == item)).any():
+                        print('id:', id, 'and item', item)
+                        relMatrix.loc[item, id] = minPuntuation
+            print(relMatrix)'''
 
-            # AVERAGE
+            # ----------------------------------- AVERAGE -----------------------------------
             if self.groupModelling.lower() == "average":
                 relMatrix["score"] = (relMatrix.sum(axis=1) / len(ids))
                 relMatrix.sort_values(by=['score'], ascending=False)
-                recommendations = list(relMatrix.index.values)
+                recommendations = list(relMatrix.index.values[:self.numOfRecommendations])
 
-            # GFAR
+            # ------------------------------------ GFAR -------------------------------------
             if self.groupModelling.lower() == "gfar":
                 gfarMatrix = relMatrix
                 for id in range(len(ids)):
@@ -56,7 +74,7 @@ class groupModeling:
                     gfarMatrix = gfarMatrix[1:]
                     gfarMatrix.drop(columns=['score'])
 
-            # GREEDYLM
+            # -------------------------------- GREEDYLM ---------------------------------
             if self.groupModelling.lower() == "greedylm":
                 bestCost = -math.inf
                 bestRecommendation = 0
@@ -93,12 +111,129 @@ class groupModeling:
                         movies = movies + trainedMovies
                     iteration += 1
 
+            # -------------------------------- SPGREEDY ------------------------------
+            if self.groupModelling.lower() == "spgreedy":
+                bestCost = -math.inf
+                bestRecommendation = 0
+                iteration = 0
+                rank = 0
+                movies = relMatrix.index.tolist()
+                trainedMovies = list()
+                satUsers = list()
+                # crete a dataframe with each user relevant films
+                for i in range(len(ids)):
+                    relMatrix.sort_values(by=[ids[i]], ascending=False)
+                    userRelItems = list(relMatrix.index.values)[:self.numRelevantItems]
+                    if i == 0:
+                        relevantItems = pd.DataFrame(userRelItems, columns=[ids[i]])
+                        continue
+                    relevantItems[ids[i]] = userRelItems
+                while rank <= self.numOfRecommendations:
+                    newRecommendation = np.random.choice(movies, size=1, replace=False).item(0)
+                    trainedMovies.append(newRecommendation)
+                    movies.remove(newRecommendation)
+                    tempSatUsers = satUsers
+                    for i in range(len(ids)):
+                        if newRecommendation in relevantItems[ids[i]]:
+                            tempSatUsers.append(ids[i])
+                    tempSatUsers = list(set(tempSatUsers))
+                    newCost = len(set(tempSatUsers) - set(satUsers))
+                    if newCost > bestCost:
+                        bestCost = newCost
+                        bestRecommendation = newRecommendation
+                        bestSatUsers = tempSatUsers
+                        # print('new recommendation', bestRecommendation, 'with cost', bestCost, 'rank', rank, 'with utility', bestTempSatUsers, 'previous recommendations', recommendations, '---------------------------------')
+                        iteration = 0
+                    if iteration >= self.numOfIterations:
+                        iteration = 0
+                        bestCost = -math.inf
+                        rank += 1
+                        satUsers = bestSatUsers
+                        recommendations.append(bestRecommendation)
+                        trainedMovies.remove(bestRecommendation)
+                        movies = movies + trainedMovies
+                    iteration += 1
+
+            # ------------------------------- FAI -----------------------------------
+            if self.groupModelling.lower() == "fai":
+                rank = 0
+                stack = []
+                # recommendationsPerUser = math.ceil(self.numOfRecommendations / self.usersPerGroup)
+                while rank <= self.numOfRecommendations:
+                    if math.floor(rank / self.usersPerGroup) == 0:
+                        availableIds = list(set(ids) - set(stack))
+                        bestRel = -math.inf
+                        for id in availableIds:
+                            column = pd.to_numeric(relMatrix[id])
+                            maxFilm = column.idxmax()
+                            maxRel = relMatrix.loc[maxFilm, id]
+                            if maxRel > bestRel:
+                                bestRel = maxRel
+                                bestFilm = maxFilm
+                                bestId = id
+                        stack.append(bestId)
+                        print('stack', stack)
+                        recommendations.append(bestFilm)
+                        rank += 1
+                    elif (math.floor(rank / self.usersPerGroup) % 2) == 0:
+                        for id in stack:
+                            print('id non reversed', id)
+                            column = pd.to_numeric(relMatrix[id])
+                            bestFilm = column.idxmax()
+                            recommendations.append(bestFilm)
+                            rank += 1
+                    else:
+                        for id in reversed(stack):
+                            print('id reversed', id)
+                            column = pd.to_numeric(relMatrix[id])
+                            bestFilm = column.idxmax()
+                            recommendations.append(bestFilm)
+                            rank += 1
+
+            # ------------------------------- XPO --------------------------------
+            if self.groupModelling.lower() == "xpo":
+                # get the N - level Pareto
+                usersTopN = list()
+                for id in ids:
+                    sorted = relMatrix.sort_values(by=[id], ascending=False)
+                    userTopN = list(sorted.head(self.numOfRecommendations).index)
+                    if len(usersTopN) == 0:
+                        usersTopN = userTopN
+                        continue
+                    usersTopN = usersTopN + userTopN
+                usersTopN = list(set(usersTopN))  # remove duplications
+                # print('usersTopN', list(usersTopN))
+                _ = relMatrix[ids]
+                xpoMatrix = _.loc[list(usersTopN), :]
+                # print(xpoMatrix)
+                # create random weights
+                filmCounts = pd.DataFrame(0, columns=["counts"], index=xpoMatrix.index)
+                for i in range(self.numOfIterations):
+                    weights = list(np.random.random(size=self.usersPerGroup))
+                    weights /= sum(weights)
+                    weightedMatrix = xpoMatrix
+                    # print(weightedMatrix)
+                    if i != 0:
+                        weightedMatrix = weightedMatrix.drop(columns=["score"])
+                        # print(weightedMatrix)
+                    weightedMatrix.mul(weights, axis=1)
+                    weightedMatrix["score"] = (weightedMatrix.sum(axis=1) / len(ids))
+                    weightedMatrix.sort_values(by=['score'], ascending=False)
+                    # print('after sort', weightedMatrix)
+                    weightedRecommendations = list(relMatrix.index.values)[:self.numOfRecommendations]
+                    for recommendation in weightedRecommendations:
+                        filmCounts.loc[recommendation, :] += 1
+                    # print("iteration", i, "filmCounts", filmCounts)
+                filmCounts.sort_values(by=['counts'], ascending=False)
+                recommendations = list(relMatrix.index.values)[:self.numOfRecommendations]
+
             # get the error
             # print(recommendations)
             metrics_ = metrics(test, ids, recommendations[:20])
             groupScores = metrics_.getScore(self.metric)
             totalScores.append((sum(groupScores) / len(ids)))
         totalScore = (sum(totalScores) / len(self.groups))
+        bar.finish()
         self.recommendationsInfo(totalScore, recommendations)
 
     def recommendationsInfo(self, totalScore, recommendations):
